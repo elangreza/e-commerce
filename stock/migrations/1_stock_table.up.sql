68ae74c0110c08fc0c95b52831075598b49b489b
mrezaelange <mrezaelange@outlook.com>
2025-09-04 17:33:32 +0700

    fix: remove unused line

--- stock/migrations/1_stock_table.up.sql
+++ stock/migrations/1_stock_table.up.sql
@@ -5,20 +5,3 @@ CREATE TABLE stocks (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-CREATE TABLE reserved_stock (
-    id INTEGER PRIMARY KEY,
-    stock_id INTEGER NOT NULL REFERENCES stocks(id),
-    quantity INTEGER NOT NULL CHECK (quantity > 0),
-    user_id TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE released_stocks (
-    id INTEGER PRIMARY KEY,
-    stock_id INTEGER NOT NULL REFERENCES stocks(id),
-    reserved_stock_id INTEGER NOT NULL REFERENCES reserved_stock(id),
-    quantity INTEGER NOT NULL CHECK (quantity > 0),
-    user_id TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
